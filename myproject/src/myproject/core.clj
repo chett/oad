f5be5fca55466bdb9276fec00db5d6716eefac2d
chett <chettttttt@gmail.com>
2010-11-15 21:13:16 -0500

    add compjure stuff

--- myproject/src/myproject/core.clj
+++ myproject/src/myproject/core.clj
@@ -1,1 +1,7 @@
-(ns myproject.core)
+(ns myproject.core
+  (:use compojure.core)
+  (:require [compojure.route :as route]))
+
+(defroutes example
+  (GET "/" [] "<h1>hello, world!</h1>")
+  (route/not-found "Page not found"))
