347c631d7b7e8536d2a1a6911d9583b00f3c7b2d
chett <chettttttt@gmail.com>
2010-11-15 21:05:58 -0500

    add compojure dep

--- myproject/project.clj
+++ myproject/project.clj
@@ -1,4 +1,6 @@
 (defproject myproject "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [compojure "0.5.2"]
+		 [ring/ring-jetty-adapter "0.3.1"]])
