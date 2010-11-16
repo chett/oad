02d9f556f3022e8dd1f91a257177cfac05099b01
chett <chettttttt@gmail.com>
2010-11-15 20:20:02 -0500

    trying leiningen

---
+++ myproject/src/myproject/core.clj
@@ -0,0 +1,1 @@
+(ns myproject.core)
---
+++ myproject/test/myproject/test/core.clj
@@ -0,0 +1,6 @@
+(ns myproject.test.core
+  (:use [myproject.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
