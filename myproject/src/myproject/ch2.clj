95e7500840def73ba1e8ef230d4630f4e10a7736
chett <chettttttt@gmail.com>
2011-01-01 22:23:42 +0000

    add simple let binding egs

--- myproject/src/myproject/ch2.clj
+++ myproject/src/myproject/ch2.clj
@@ -1,9 +1,33 @@
 ;; Programming Clojure Chapter 1 examples
 ;; 12-28-2010
-(ns myproject.ch2)
+(ns myproject.ch2
+  (:use [clojure.contrib.str-utils :only (re-split)]))
 
 (defn encode [x]
   (apply str (apply interleave x)))
 
 (defn argfn [z & q]
   (apply str z q))
+(def inventors {:Lisp "McCarthy", :Clojure "Hickey"})
+(defstruct book :title :author)
+
+(defn hello-arity
+  ([] (hello-arity "world"))
+  ([x] (str "Hello, " x)))
+
+(defn indexable-word? [x]
+  (> (count x) 2))
+
+
+
+(defn long-filter []
+  (filter indexable-word?
+	  (re-split #"\W" "A quick fox jumped over a brown  dog")))
+
+(defn indexable-words [text]
+  (let [indexable-word? (fn [w] (> (count w) 2))
+	split-on-whitespace (fn [s] (re-split #"\W+" s))]
+    (filter indexable-word? (split-on-whitespace text))))
+
+(defn make-greeter [greeting-prefix]
+  #(str greeting-prefix ", " %))
--- myproject/test/myproject/test/ch2test.clj
+++ myproject/test/myproject/test/ch2test.clj
@@ -1,8 +1,31 @@
 (ns myproject.test.ch2test
   (:use [myproject.ch2] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use [clojure.contrib.str-utils :only (re-split)]))
 
 ;;pg 31
 (deftest encode-test
   (is (= (encode ["xxx" "yyy" "zzz"])
 	 "xyzxyzxyz")))
+
+;;pg 37
+(deftest hello-arity-test
+  (is (.equals "Hello, world" (hello-arity))))
+
+;;pg 38
+(deftest filter-test
+  (is (= (long-filter)
+	 (filter (fn [w] (> (count w) 2))
+		 (re-split #"\W" "A quick fox jumped over a brown  dog")))))
+;;pg 39
+(deftest filter-test-2
+  (is (= (filter #(> (count %) 2)
+		 (re-split #"\W" "A quick fox jumped over a brown  dog"))
+	 (long-filter)
+	 (indexable-words "A quick fox jumped over a brown  dog"))))
+
+;;pg 40
+(deftest greeter-ter
+  (let [hawaiian-greeter (make-greeter "Aloha")]
+    (is (= (hawaiian-greeter "ted")
+	   "Aloha, ted"))))
